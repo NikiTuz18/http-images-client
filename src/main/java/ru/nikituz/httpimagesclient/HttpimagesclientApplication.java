5c0557cd3fe2fdeb145fb5b03bfc6415095cf237
NikiTuz <nevenchanniy.egor.90@gmail.com>
2024-02-20 12:05:24 +0300

    init application

---
+++ src/main/java/ru/nikituz/httpimagesclient/HttpimagesclientApplication.java
@@ -0,0 +1,13 @@
+package ru.nikituz.httpimagesclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpimagesclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpimagesclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nikituz/httpimagesclient/HttpimagesclientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nikituz.httpimagesclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpimagesclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
